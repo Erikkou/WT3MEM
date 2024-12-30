d40680f64b635d33739c74fa71d75309eb7a48db
Erikkou <e.p.kouwenhoven@st.hanze.nl>
2024-12-30 17:13:26 +0100

    Mobilefriendly

--- js/modules/settings.js
+++ js/modules/settings.js
@@ -1,9 +1,10 @@
-
+export const BACKEND_URL = 'http://localhost:8000';
 
 export function initSettings(gameBoard) {
     sizeChecker();
     cardCharacterChecker();
     setupNewGameButton(gameBoard);
+    initLeaderboard();
   }
  
   
@@ -39,4 +40,40 @@ function cardCharacterChecker() {
   cardCharacter.addEventListener('change', () => {
     console.log('New card character:', cardCharacter.value);
   });
+}
+
+async function getScores() {
+  const response = await fetch(`${BACKEND_URL}/scores`);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const scores = await response.json();
+  return scores;
+}
+
+function sortScores(scores) {
+  return scores
+  .sort((a, b) => a.score - b.score)
+  .reduce((acc, score, index) => {
+    if (index < 5) {
+      acc.push(score);
+    }
+    return acc;
+  }, []);
+}
+
+function makeLeaderboard(scores) {
+  const leaderboard = document.getElementById('leaderboard');
+  leaderboard.innerHTML = '';
+  scores.forEach(score => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${score.name} - ${score.score}`;
+    leaderboard.appendChild(listItem);
+  });
+}
+
+async function initLeaderboard() {
+  const scores = await getScores();
+  const topScores = sortScores(scores);
+  makeLeaderboard(topScores);
 }
